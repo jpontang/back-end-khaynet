bf0f57813b61e234a040a82c6dcc348544d780eb
jpontang <85880792+jpontang@users.noreply.github.com>
2024-09-09 20:26:57 +0700

    commit

---
+++ database/seeders/RoleTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        
+        $user = User::create([
+            'name' => 'Admin Full',
+            'email' => 'admin4@gmail.com',
+            'password' => bcrypt('password'),
+            'level' => 'admin'
+        ]);
+
+       // $role = Role::create(['name' => 'admin']);
+
+        $permissions = Permission::pluck('id','id')->all();
+
+        $role->syncPermissions($permissions);
+
+        $user->assignRole(1);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Role\RoleController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+// header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
+
+// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware(['auth:sanctum'])->group(function(){
+    Route::get('/user', function ()
+    {
+        return auth()->user();
+    });
+
+    Route::apiResource('roles', RoleController::class);
+});
+
+//Route::apiResource('roles', RoleController::class)->middleware('auth:sanctum');
