dd742bf6296977c3a6a28844ff5623215fe52a77
jpontang <jpontang@gmail.com>
2024-09-10 18:25:19 +0700

    add

---
+++ routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Role\RoleController;
+use App\Http\Controllers\Captcha\CaptchaController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+// header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
+
+// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware(['auth:sanctum'])->group(function(){
+    Route::get('/user', function ()
+    {
+        return auth()->user();
+    });
+
+    Route::apiResource('roles', RoleController::class);
+});
+
+Route::get('captcha', [CaptchaController::class,'index' ])->name('captcha.index');
+
+//Route::apiResource('roles', RoleController::class)->middleware('auth:sanctum');
